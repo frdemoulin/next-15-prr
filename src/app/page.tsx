dfcd6c77bc2bbdbcb4857a3b088293fb48da700b
frdemoulin <freddmn08@gmail.com>
2024-08-22 15:07:20 +0200

    fix URL top-rated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl font-bold text-blue-500">Next.js 15 rendering experimentation</h1>
         <ul className="space-y-2">
-          <li><Link href="/movies" className="hover:underline">Movies list (with PPR)</Link></li>
+          <li><Link href="/top-rated" className="hover:underline">Top rated movies and TV shows (with PPR)</Link></li>
           <li><Link href="/about" className="hover:underline">About (with SSR)</Link></li>
         </ul>
       </main>
